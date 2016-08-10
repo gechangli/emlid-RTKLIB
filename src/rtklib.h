24344c4cace777bc7834c8186188fec5db678dd2
Egor Fedorov <egor.fedorov@emlid.com>
2016-08-10 15:58:50 +0300

    Added SOLF_ERB define and outerb function declaration

--- src/rtklib.h
+++ src/rtklib.h
@@ -349,6 +349,7 @@ extern "C" {
 #define SOLF_NMEA   3                   /* solution format: NMEA-183 */
 #define SOLF_STAT   4                   /* solution format: solution status */
 #define SOLF_GSIF   5                   /* solution format: GSI F1/F2 */
+#define SOLF_ERB    6                   /* solution format: ERB protocol */
 
 #define SOLQ_NONE   0                   /* solution status: no solution */
 #define SOLQ_FIX    1                   /* solution status: fix */
@@ -1690,6 +1691,7 @@ EXPORT int outnmea_gsa(unsigned char *buff, const sol_t *sol,
                        const ssat_t *ssat);
 EXPORT int outnmea_gsv(unsigned char *buff, const sol_t *sol,
                        const ssat_t *ssat);
+EXPORT int outerb(unsigned char *buff, const sol_t *sol);
 
 /* google earth kml converter ------------------------------------------------*/
 EXPORT int convkml(const char *infile, const char *outfile, gtime_t ts,
