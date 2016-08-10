f2fcc6961f7cdc76487fed30462291cb197df656
Egor Fedorov <egor.fedorov@emlid.com>
2016-08-10 19:23:52 +0300

    Added event name, epoch flag, time status, counts of time marks and counts of receiver events to the corresponding structures

--- src/rtklib.h
+++ src/rtklib.h
@@ -517,6 +517,8 @@ typedef struct {        /* time struct */
 
 typedef struct {        /* observation data record */
     gtime_t time;       /* receiver sampling time (GPST) */
+    gtime_t eventime;   /* time of event (GPST) */
+    int timevalid;      /* time is valid (Valid GNSS fix) for time mark */
     unsigned char sat,rcv; /* satellite/receiver number */
     unsigned char SNR [NFREQ+NEXOBS]; /* signal strength (0.25 dBHz) */
     unsigned char LLI [NFREQ+NEXOBS]; /* loss of lock indicator */
@@ -528,6 +530,9 @@ typedef struct {        /* observation data record */
 
 typedef struct {        /* observation data */
     int n,nmax;         /* number of obervation data/allocated */
+    int flag;           /* epoch flag (0:ok,1:power failure,>1:event flag) */
+    int rcvcount;       /* count of rcv event */
+    int tmcount;        /* time mark count */
     obsd_t *data;       /* observation data records */
 } obs_t;
 
